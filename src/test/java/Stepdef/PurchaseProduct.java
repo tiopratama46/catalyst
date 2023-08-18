73625405a5e38dc1ac72fe0d6e0c55767147640e
Tio pratama agung <141820829+tiopratama46@users.noreply.github.com>
2023-08-18 15:29:29 +0700

    Update PurchaseProduct.java

--- src/test/java/Stepdef/PurchaseProduct.java
+++ src/test/java/Stepdef/PurchaseProduct.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
